f37eb7d93397daa5a1c444d4ab137da33c82cec9
dmzapatas1 <dmzapatas10@gmail.com>
2023-10-17 17:11:14 -0500

    first approach task 2

--- test/specs/test.e2e.js
+++ test/specs/test.e2e.js
@@ -1,4 +1,4 @@
-const { expect, browser, $, toHaveHref } = require('@wdio/globals')
+const { expect, browser, $ , $$} = require('@wdio/globals')
 const { it } = require('mocha')
 
 // describe('My Login application', () => {
@@ -15,7 +15,9 @@ const { it } = require('mocha')
 //     })
 // })
 
-describe("Test suite", () => {
+//TASK 1
+
+describe("Test suite - task 1", () => {
     it("first test", async () =>{
         await browser.url("https://es.wikipedia.org/wiki/Wikipedia:Portada");
         const pageTitle = await browser.getTitle();
@@ -24,29 +26,72 @@ describe("Test suite", () => {
     })
 
     it("Second test", async () =>{
-        
-        await browser.url("https://es.wikipedia.org/wiki/Wikipedia:Portada");
         const path = await $('//*[@id="Portales"]')
         expect(await path.getText()).toEqual('Portales')
     })
 
     it("Third test", async () =>{
-        
-        await browser.url("https://es.wikipedia.org/wiki/Wikipedia:Portada");
         const path = $('//*[@id="Recurso_del_día"]/a')
         expect(await path.getAttribute('href')).toHaveHref('/wiki/Wikipedia:Recurso_del_d%C3%ADa')
     })
  
     it("Fourth test", async () =>{ 
-        await browser.url("https://es.wikipedia.org/wiki/Wikipedia:Portada");
         const image = $('//*[@id="main-tfa"]/p[1]/a[1]')
         expect(await image.waitForDisplayed()).toBeClickable()
         
     })
     it("Fifth test", async () =>{ 
-        await browser.url("https://es.wikipedia.org/wiki/Wikipedia:Portada");
         const image = $('#main-tfa > figure > a > img')
         expect(await image.getAttribute('class')).toContain('mw-file-element')
     })
 })
+
+//TASK 2
+
+describe("Test suite - task 2", () => {
+    it("first test", async () =>{
+        await browser.url("https://www.casadellibro.com.co/");
+        const layout = await $('#__layout')
+        expect(await layout.isExisting()).toEqual(true)
+    })
+    it("second test", async () =>{
+        const main = await $('.v-main')
+        expect(await main.isDisplayed()).toEqual(true)
+    })
+    it("third test", async () =>{
+        const input = await $('//*[@id="app"]/div[1]/div[1]/div[2]/div[2]/div/input')
+        expect(await input.setValue('James Clear'))
+        await browser.pause(5000)
+    })
+    it("fourth test", async () =>{
+        const bag = await $('//*[@id="app"]/div[1]/div[1]/div[2]/div[1]/button[2]')
+        await bag.waitForDisplayed({
+            timeout: 3000,
+            interval: 50
+        })   
+    })
+    it("fifth test", async () =>{
+        const div = await $('#onetrust-accept-btn-handler')
+        await browser.waitUntil(async () => {
+          return await div.click();
+        }, {
+          timeout: 10000,
+          timeoutMsg: 'Expected div to disappear within 10s'
+        });
+      await div.click()
+     })
+//     it("r", async () =>{
+//         const text = await $('//*[@id="app"]/div[1]/main/div/div/div/div[16]/div')
+//         //*[@id="app"]/div[1]/main/div/div/div/div[16]/div/div[1]
+//         //*[@id="app"]/div[1]/main/div/div/div/div[16]/div/div[1]/h2
+//         const div = await (await text.$$('div')[1].$('h2')).isExisting()
+//         expect(div).toEqual(false)
+      
+//     })
+})
+
+// TASK 3
+
+
+
 
